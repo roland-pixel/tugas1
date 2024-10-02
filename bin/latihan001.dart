37d614122627a09d5e5c5da5e02499d0fef091d4
roland-pixel <kharis7181@gmail.com>
2024-10-02 08:10:40 +0700

    initial commit, buat folder khusus tugas, buat file variabel(latihan001)

---
+++ bin/latihan001.dart
@@ -0,0 +1,22 @@
+void main() {
+  // kode tanpa variabel
+  print("Kharis Raihan");
+  print("Kharis Raihan");
+  print("Kharis Raihan");
+  print("Kharis Raihan");
+
+  // variabel
+  // String nama;
+  // nama = "Kharis Raihan";
+  // print(nama);
+  // print(nama);
+  // print(nama);
+  // print(nama);
+
+  // deklarasi variabel langsung
+  String nama = "Kharis Raihan123";
+  print(nama);
+  print(nama);
+  print(nama);
+  print(nama);
+}
