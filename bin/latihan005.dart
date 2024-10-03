dd45d1e75d49623d8808eb1a1ce3b6864de4d673
roland-pixel <kharis7181@gmail.com>
2024-10-03 19:29:02 +0700

    tipe data string

--- bin/latihan005.dart
+++ bin/latihan005.dart
@@ -1,3 +1,10 @@
 void main() {
   // string
+//   String merupakan tipe data text atau tulisan
+// Untuk membuat String, kita bisa menggunakan tanda kutip satu atau kutip dua, lalu di dalamnya  berisi nilai text nya
+// Walaupun String bisa menggunakan kutip dua, tapi disarankan untuk menggunakan kutip satu saja
+  String namadepan = 'kharis';
+  String namabelakang = "Raihan";
+  print(namadepan);
+  print(namabelakang);
 }
