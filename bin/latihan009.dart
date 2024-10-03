d36f7f84f9ed8d6ad0668e4bdb598975763e5df6
roland-pixel <kharis7181@gmail.com>
2024-10-03 20:18:54 +0700

    contoh operator penugasan

--- bin/latihan009.dart
+++ bin/latihan009.dart
@@ -1,3 +1,23 @@
 void main() {
   // operator perbandingan
+  // Operator perbandingan adalah operator yang hasilnya adalah bool
+  // Operator perbandingan bisa digunakan di tipe data number dan string
+  // == adalah sama dengan
+  // != adalah tidak sama dengan
+  // > adalah lebih dari
+  // < adalah kurang dari
+  // >= adalah lebih dari atau sama dengan
+  // <= adalah kurang dari atau sama dengan
+  var hasil1 = 'kharis' == 'Kharis';
+  var hasil2 = 20 != 15;
+  var hasil3 = 20 > 15;
+  var hasil4 = 20 < 15;
+  var hasil5 = 20 >= 20;
+  var hasil6 = 20 <= 15;
+  print(hasil1);
+  print(hasil2);
+  print(hasil3);
+  print(hasil4);
+  print(hasil5);
+  print(hasil6);
 }
