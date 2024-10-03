06aeb18f924dd8bd96cc408f4add70c0b12be5eb
roland-pixel <kharis7181@gmail.com>
2024-10-03 19:26:16 +0700

    tipe data booleam

--- bin/latihan004.dart
+++ bin/latihan004.dart
@@ -1,1 +1,10 @@
-void main() {}
+void main() {
+  // Boolean adalah tipe data yang hanya memiliki dua nilai, yaitu benar atau salah
+// Boolean direpresentasikan dengan kata kunci bool
+  // contoh
+  bool selesai = false;
+  print(selesai);
+
+  selesai = true;
+  print(selesai);
+}
