fe7b506ac58e98a3d2d1d8a630f886773269bc28
roland-pixel <kharis7181@gmail.com>
2024-10-03 20:12:26 +0700

    contoh operator aritmatika

--- bin/latihan008.dart
+++ bin/latihan008.dart
@@ -1,3 +1,26 @@
 void main() {
   // operator aritmatika
+  // + untuk pertambahan
+  // - untuk pengurangan juga bisa untuk menjadi angka negatif
+  // * untuk perkalian
+  // / untuk pembagian, hasil double
+  // ~/ untuk pembagian, hasil int
+  // % untuk sisa bagi
+  var hasil1 = 10 + 10;
+  var hasil2 = 100 - 10;
+  var hasil3 = -5;
+  var hasil4 = 7;
+  var hasil5 = 10 * 4;
+  var hasil6 = 35 / 3;
+  var hasil7 = 46 ~/ 7;
+  var hasil8 = 10 % 3;
+
+  print(hasil1);
+  print(hasil2);
+  print(hasil3);
+  print(hasil4);
+  print(hasil5);
+  print(hasil6);
+  print(hasil7);
+  print(hasil8);
 }
