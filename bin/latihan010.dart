20db2dbbc6ea518297f90547fdc90ba9117633fb
roland-pixel <kharis7181@gmail.com>
2024-10-03 20:25:59 +0700

    contoh operator penugasan

--- bin/latihan010.dart
+++ bin/latihan010.dart
@@ -1,3 +1,16 @@
 void main() {
-  // operator penugasan
+  // a = a + 10 sama saja dengan a+=10;
+  // a = a - 10 sama saja dengan a-=10;
+  // a = a * 10 sama saja dengan a*=10;
+  // a = a / 10 sama saja dengan a/=10;
+  // a = a ~/ 10 sama saja dengan a~/=10;
+  // a = a % 10 sama saja dengan a%=10;
+  dynamic a = 35;
+  a += 10;
+  a -= 10;
+  a *= 10;
+  a /= 10;
+  a ~/= 10;
+  a %= 10;
+  print(a);
 }
