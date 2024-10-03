3eacbc3a917ae02e58503061f0f9222db9314120
roland-pixel <kharis7181@gmail.com>
2024-10-03 20:31:53 +0700

    increment dan decrement

--- bin/latihan010.dart
+++ bin/latihan010.dart
@@ -7,10 +7,30 @@ void main() {
   // a = a % 10 sama saja dengan a%=10;
   dynamic a = 35;
   a += 10;
+  print(a);
   a -= 10;
+  print(a);
   a *= 10;
+  print(a);
   a /= 10;
+  print(a);
   a ~/= 10;
+  print(a);
   a %= 10;
   print(a);
+
+  // increment dan decrement
+  // ++var sama saja dengan var = var + 1 (expression is var + 1)
+  // var++ sama saja dengan var = var + 1 (expression is var )
+  // --var sama saja dengan var = var - 1 (expression is var - 1)
+  // var-- sama saja dengan var = var - 1 (expression is var )
+  var var1 = 10;
+  ++var1;
+  print(var1);
+  var1++;
+  print(var1);
+  --var1;
+  print(var1);
+  var1--;
+  print(var1);
 }
