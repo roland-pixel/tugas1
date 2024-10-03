788a21901c338513d35cf6f6b7bb68c780471ec0
roland-pixel <kharis7181@gmail.com>
2024-10-03 19:58:13 +0700

    konversi angka ke string dan sebaliknya

--- bin/latihan007.dart
+++ bin/latihan007.dart
@@ -1,3 +1,27 @@
 void main() {
   // konversi tope data
+  // Kadang ada kebutuhan kita melakukan konversi tipe data, terutama dari tipe data String ke Number dan Boolean, atau bahkan kebalikannya
+  // Hal ini biasa kita lakukan ketika misal menerima input dari pengguna aplikasi kita, dimana biasanya inputnya dalam bentuk String, dan kita butuh melakukan konversi ke tipe data yang kita inginkan, misal Number atau Boolean
+
+  // konversi number dan string
+  // Dart merupakan bahasa pemrograman berorientasi objek, semua tipe data di Dart adalah object, dimana object memiliki method/function
+  // Kita bisa menggunakan method toString() untuk melakukan konversi dari Number ke String
+  // Sedangkan untuk melakukan konversi dari String  ke Number, kita bisa gunakan method parse(), baik itu di int ataupun di double
+  // Sedangkan jika kita ingin melakukan konversi dari Number ke Number lain, kita bisa gunakan  method toInt() atau toDouble()
+  var inputstring = '1000';
+  var inputint = int.parse(inputstring);
+  var inputdouble = double.parse(inputstring);
+
+  var doubledariint = inputint.toDouble();
+  var intdaridouble = inputdouble.toInt();
+
+  var stringdariint = inputint.toString();
+  var stringdaridouble = inputdouble.toString();
+
+  print('string to int = $inputint');
+  print('string to double = $inputdouble');
+  print('int to double = $doubledariint');
+  print('double to int = $intdaridouble');
+  print('int to string = $stringdariint');
+  print('double to string = $stringdaridouble');
 }
