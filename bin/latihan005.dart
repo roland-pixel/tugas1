bb2c79bb3acfeeff63105f02c528b043c9de0146
roland-pixel <kharis7181@gmail.com>
2024-10-03 19:35:31 +0700

    karakter backslash

--- bin/latihan005.dart
+++ bin/latihan005.dart
@@ -11,7 +11,12 @@ void main() {
   // string interpolation
   // String mendukung expression, dimana di dalam expression kita bisa mengambil data dari variable lain
   // Untuk membuat expression, kita bisa menggunakan format ${isiExpression}, jika sederhana kita bisa langsung menggunakan $isiExpression
-
   var namalengkap = '$namadepan $namabelakang';
   print(namalengkap);
+
+  // karakter backslash
+  // Karakter \ (backslash) di String bisa digunakan untuk menekankan bahwa karakter setelahnya dianggap benar karakter tersebut
+  // Seperti contohnya sebelumnya karakter $ dianggap expression, jika kita memang mau membuat karakter $ dalam string, maka kita bisa gunakan \$, atau jika kita mau membuat karakter ‘ (petik satu), kita bisa gunakan \’
+  var text = 'this is \'dart\' \$cool';
+  print(text);
 }
