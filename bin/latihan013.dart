3ffc2d193f16ae3aa44cf484973fa9bb6fff85bd
roland-pixel <kharis7181@gmail.com>
2024-10-04 15:59:06 +0700

    membuat list

--- bin/latihan013.dart
+++ bin/latihan013.dart
@@ -1,3 +1,18 @@
 void main() {
   // List
+  // List merupakan tipe data yang berisikan kumpulan data
+  // Di bahasa pemrograman lain, tipe data ini dikenal dengan tipe data Array
+  // Saat kita membuat List, kita perlu menentukan isi dari tipe data List
+  // Untuk membuat data List, kita bisa menggunakan []
+  // Di  Dart, semua tipe data adalah object, dimana List sendiri memiliki property, method dan operator
+
+  // membuat list
+  // Untuk membuat list, kita bisa tentukan tipe datanya, misal :List<TipeData> namaVariable = [];
+  // Atau bisa menggunakan kata kunci var atau final :var namaVariable = <TipeData>[];final namaVariable = <TipeData>[];
+
+  // membuat list dengan tipe data int
+  List<int> listint = [];
+
+  // membuat list dari string
+  var listString = <String>[];
 }
