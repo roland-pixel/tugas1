7b7b177c8e825f735a3bb8984c0081e409b12d6d
roland-pixel <kharis7181@gmail.com>
2024-10-04 15:53:21 +0700

    contoh operator type test

--- bin/latihan012.dart
+++ bin/latihan012.dart
@@ -1,3 +1,14 @@
 void main() {
   // operator type test
+  // as     Typecast, melakukan konversi tipe data secara paksa
+  // is     true, jika object sesuai tipe data
+  // is!    true, jika object tidak sesuai tipe data
+
+  dynamic variabel = 500;
+  var variabelint = variabel as int;
+  var isint = variabel is int;
+  var isnotbool = variabel is! bool;
+  print(variabelint);
+  print(isint);
+  print(isnotbool);
 }
