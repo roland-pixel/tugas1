6044c9b1332a14b21a9c0e3925df21dcd0599d4b
roland-pixel <kharis7181@gmail.com>
2024-10-06 05:26:55 +0700

    contoh while loop

--- bin/latihan023.dart
+++ bin/latihan023.dart
@@ -2,4 +2,9 @@ void main() {
   // while loop
   // While loop adalah versi perulangan yang lebih sederhana dibanding for loop
   // Di while loop, hanya terdapat kondisi perulangan, tanpa ada init statement dan post statement
+  var counter = 1;
+  while (counter <= 10) {
+    print('perulangan ke $counter');
+    counter++;
+  }
 }
