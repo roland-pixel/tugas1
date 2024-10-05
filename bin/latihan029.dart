fc48399e9c653591d53bdd28b930faa8b230da71
roland-pixel <kharis7181@gmail.com>
2024-10-06 05:55:16 +0700

    kode optional parameter

--- bin/latihan029.dart
+++ bin/latihan029.dart
@@ -1,6 +1,12 @@
+void say_hello(String namadepan, [String? namabelakang]) {
+  print('hello $namadepan $namabelakang');
+}
+
 void main() {
   // optional parameter
   // Secara default, parameter wajib dikirim ketika kita memanggil function
   // Namun jika kita ingin membuat parameter yang optional, artinya tidak wajib dikirim, kita bisa wrap dalam  kurung []
   // Dan parameter optional haruslah nullable
+  say_hello('kharis');
+  say_hello('budi', 'setiawan');
 }
