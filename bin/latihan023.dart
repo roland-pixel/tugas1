a010a2396a8fc82cdc5081a4a292007a1ff27e06
roland-pixel <kharis7181@gmail.com>
2024-10-06 05:25:40 +0700

    initial commit, while loop

---
+++ bin/latihan023.dart
@@ -0,0 +1,5 @@
+void main() {
+  // while loop
+  // While loop adalah versi perulangan yang lebih sederhana dibanding for loop
+  // Di while loop, hanya terdapat kondisi perulangan, tanpa ada init statement dan post statement
+}
