22908f89f331dd7b4705b753e5f6ae40b7487417
roland-pixel <kharis7181@gmail.com>
2024-10-05 18:48:04 +0700

    initial commit, null

---
+++ bin/latihan019.dart
@@ -0,0 +1,6 @@
+void main() {
+  // Kata kunci null merupakan tipe data representasi data kosong
+  // Secara default, saat kita membuat variable, data harus diisi, jika tidak diisi, maka variable tidak bisa digunakan
+  // Namun jika kita ingin membuat sebuah variable bisa diisi data null, kita bisa tambahkan karakter ? (tanda tanya), seperti :
+  // TipeData? namaVariable  = null;
+}
