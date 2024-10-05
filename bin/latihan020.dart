9b71828e58fe27680cb5a812168b3ce2a983e488
roland-pixel <kharis7181@gmail.com>
2024-10-05 18:56:28 +0700

    contoh ternary operator

--- bin/latihan020.dart
+++ bin/latihan020.dart
@@ -1,3 +1,23 @@
 void main() {
   // terbary operator
+  // Ternary operator adalah operator sederhana dari if
+  // Ternary operator terdiri dari  kondisi yang dievaluasi, jika menghasilkan true maka nilai pertama diambil, jika false, maka nilai kedua diambil
+
+  // kode tanpa ternary operator
+  // var nilai = 75;
+  // String ucapan;
+  // if (nilai >= 75) {
+  //   ucapan = 'selamat, anda lulus, ini iwak nya';
+  // } else {
+  //   ucapan = 'maaf anda gagal, coba lagi';
+  // }
+  // print(ucapan);
+
+  // kode ternary operator
+  var nilai = 75;
+  var ucapan = nilai >= 80
+      ? 'selamat, anda lulus, ini iwak nya'
+      : 'maaf anda gagal, coba lagi';
+
+  print(ucapan);
 }
