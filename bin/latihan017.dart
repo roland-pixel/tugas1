552ee3610cce9ab745eaa34281fa08a14ddbcf25
roland-pixel <kharis7181@gmail.com>
2024-10-05 09:18:50 +0700

    contoh if

--- bin/latihan017.dart
+++ bin/latihan017.dart
@@ -1,3 +1,13 @@
 void main() {
   // if dan else
+  // Dalam Dart, if adalah salah satu kata kunci yang digunakan untuk percabangan
+  // Percabangan artinya kita bisa mengeksekusi kode program tertentu ketika suatu kondisi terpenuhi
+  // Hampir di semua bahasa pemrograman mendukung percabagan if
+
+  // kode if
+  var nilai = 70;
+  var absen = 90;
+  if (nilai >= 75 && absen >= 75) {
+    print('anda lulus');
+  }
 }
