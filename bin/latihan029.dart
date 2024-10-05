84f61cfd169d3f066c51525f01a3472f6949b4e0
roland-pixel <kharis7181@gmail.com>
2024-10-06 05:52:52 +0700

    initial commit, optional parameter

---
+++ bin/latihan029.dart
@@ -0,0 +1,6 @@
+void main() {
+  // optional parameter
+  // Secara default, parameter wajib dikirim ketika kita memanggil function
+  // Namun jika kita ingin membuat parameter yang optional, artinya tidak wajib dikirim, kita bisa wrap dalam  kurung []
+  // Dan parameter optional haruslah nullable
+}
