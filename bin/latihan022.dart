dd97d8905db30d6698a827e5246e998ad5e4f2bb
roland-pixel <kharis7181@gmail.com>
2024-10-05 19:17:04 +0700

    syntax perulangan for

--- bin/latihan022.dart
+++ bin/latihan022.dart
@@ -1,3 +1,15 @@
 void main() {
   // for loop
+  // For adalah salah satu kata kunci yang bisa digunakan untuk melakukan perulangan
+  // Blok kode yang terdapat di dalam for akan selalu diulangi selama kondisi for terpenuhi
+
+  // Sintak Perulangan For
+  // for(init statement; kondisi; post statement){
+  // block perulangan
+  // }
+
+  // Init statement akan dieksekusi hanya sekali di awal sebelum perulangan
+  // Kondisi akan dilakukan pengecekan dalam setiap perulangan, jika true perulangan akan dilakukan, jika false perulangan akan berhenti
+  // Post statement akan dieksekusi setiap kali diakhir perulangan
+  // Init statement, Kondisi dan Post Statement tidak wajib diisi, jika Kondisi tidak diisi, berarti kondisi selalu bernilai true
 }
