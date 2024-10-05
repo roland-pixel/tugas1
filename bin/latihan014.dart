47505d2ad51e1e57c11f12a06e5d9286f291d224
roland-pixel <kharis7181@gmail.com>
2024-10-05 08:46:45 +0700

    contoh set

--- bin/latihan014.dart
+++ bin/latihan014.dart
@@ -1,3 +1,15 @@
 void main() {
   // set
+  // Set merupakan tipe data sama seperti List, namun ada beberapa hal yang berbeda dengan List
+  // Set tidak menerima duplikat data, artinya jika kita memasukkan data duplikat, hanya satu yang diterima, yang lainnya akan dihiraukan
+  // Set tidak menjamin urutan data, jika dalam List, urutan data sudah pasti menggunakan index, pada Set tidak ada index
+
+  // membuat set
+  // Untuk membuat Set, kita menggunakan {} (kurung kurawal )
+  // Set<TipeData> namaVariable = {};
+  // var namaVariable = <TipeData>{};
+
+  // contoh
+  Set<int> numbers = {};
+  var names = <String>{};
 }
