c67fbf562b6d6dfaf28865a47a10a2d4d183fbde
roland-pixel <kharis7181@gmail.com>
2024-10-05 09:26:45 +0700

    else if

--- bin/latihan017.dart
+++ bin/latihan017.dart
@@ -16,4 +16,20 @@ void main() {
   // Blok if akan dieksekusi ketika kondisi if bernilai true
   // Kadang kita ingin melakukan eksekusi program tertentu jika kondisi if bernilai false
   // Hal ini bisa dilakukan menggunakan else
+
+  // Kada dalam If, kita butuh membuat beberapa kondisi
+  // Kasus seperti ini, kita bisa menggunakan else if
+  // Else if bisa ditambahkan sebanyak-banyaknya
+  // Ketika salah satu kondisi terpenuhi, maka otomatis akan berhenti, if else selanjutnya tidak akan dieksekusi
+  if (nilai >= 80 && absen >= 80) {
+    print('nilai anda A');
+  } else if (nilai >= 70 && absen >= 70) {
+    print('nilai anda B');
+  } else if (nilai >= 60 && absen >= 60) {
+    print('nilai anda C');
+  } else if (nilai >= 50 && absen >= 50) {
+    print('nilai anda C');
+  } else {
+    print('nilai anda E');
+  }
 }
