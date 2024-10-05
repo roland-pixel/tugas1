17f4fa85e319f858ca8bb427dd53540deb91c758
roland-pixel <kharis7181@gmail.com>
2024-10-05 08:53:20 +0700

    deklarasi set secara langsung

--- bin/latihan014.dart
+++ bin/latihan014.dart
@@ -26,4 +26,9 @@ void main() {
   print(names);
   names.remove('Raihan');
   print(names);
+
+  // deklarasi set seara langsung
+  // Set juga mendukung deklarasi data secara langsung ketika pembuatan variable Set
+  // Ini mempermudah kita ketika membuat Set jika memang datanya sudah siap
+  // Caranya bisa gunakan perintah :var namaVariable = {value1, value2, value3}var namaVariable = <TipeData>{value1, value2, value3}var namaVariable = {	value1,	value2,	value3,}
 }
