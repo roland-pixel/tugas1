896cefd4fc3d3e213700174dd1e5be5fbf222644
roland-pixel <kharis7181@gmail.com>
2024-10-05 18:49:03 +0700

    contoh null

--- bin/latihan019.dart
+++ bin/latihan019.dart
@@ -3,4 +3,9 @@ void main() {
   // Secara default, saat kita membuat variable, data harus diisi, jika tidak diisi, maka variable tidak bisa digunakan
   // Namun jika kita ingin membuat sebuah variable bisa diisi data null, kita bisa tambahkan karakter ? (tanda tanya), seperti :
   // TipeData? namaVariable  = null;
+
+  int number1;
+  int? number2;
+  // print(number1);
+  print(number2);
 }
