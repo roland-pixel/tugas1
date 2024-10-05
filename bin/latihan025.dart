bda5c9d5d9ab32fb3a410decdf58197d90e0eda0
roland-pixel <kharis7181@gmail.com>
2024-10-06 05:33:53 +0700

    kode break

--- bin/latihan025.dart
+++ bin/latihan025.dart
@@ -1,3 +1,17 @@
 void main() {
   //  break and continue
+  // Pada switch case, kita sudah mengenal kata kunci break, yaitu untuk menghentikan case dalam switch
+  // Sama dengan pada perulangan, break juga digunakan untuk menghentikan seluruh perulangan.
+  // Namun berbeda dengan continue, continue digunakan untuk menghentikan perulangan saat ini, lalu melanjutkan ke perulangan selanjutnya
+
+  // kode break
+  var counter = 1;
+  while (true) {
+    print('perulangan ke $counter');
+    counter++;
+
+    if (counter > 10) {
+      break;
+    }
+  }
 }
