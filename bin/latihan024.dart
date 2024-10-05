9fbc854d3473d9acabf7f5d8d44e990fde32fe89
roland-pixel <kharis7181@gmail.com>
2024-10-06 05:30:31 +0700

    contoh do while loop

--- bin/latihan024.dart
+++ bin/latihan024.dart
@@ -1,3 +1,12 @@
 void main() {
   // do while loop
+  // Do While loop adalah perulangan yang mirip dengan while
+  // Perbedaannya hanya pada pengecekan kondisi
+  // Pengecekan kondisi di while loop dilakukan di awal sebelum perulangan dilakukan, sedangkan di do while loop dilakukan setelah perulangan dilakukan
+  // Oleh karena itu dalam do while loop, minimal pasti sekali perulangan dilakukan walaupun kondisi tidak bernilai true
+  var counter = 100;
+  do {
+    print('perulangan ke $counter');
+    counter++;
+  } while (counter <= 10);
 }
