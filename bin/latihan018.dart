e21c6e2c2307927fd126df6d3f0b7449106670b8
roland-pixel <kharis7181@gmail.com>
2024-10-05 09:33:01 +0700

    contoh case

--- bin/latihan018.dart
+++ bin/latihan018.dart
@@ -1,3 +1,21 @@
 void main() {
   // switch Case
+  // Kadang kita hanya butuh menggunakan kondisi sederhana di if, seperti hanya menggunakan perbandingan ==
+  // Switch adalah statement percabangan yang sama dengan if, namun  lebih sederhana cara pembuatannya
+  // Kondisi di switch statement hanya untuk perbandingan ==
+  var nilai = 'A';
+  switch (nilai) {
+    case 'A':
+      print('wow anda lulus, ini iwak anda ');
+      break;
+    case 'B':
+    case 'C':
+      print('Anda lulus');
+      break;
+    case 'D':
+      print('Anda tidak lulus');
+      break;
+    default:
+      print('Mungkin Anda Salah Jurusan');
+  }
 }
