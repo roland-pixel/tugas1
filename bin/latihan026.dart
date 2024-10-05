ef936254cdabf806792ddaf906f6f35909411f6a
roland-pixel <kharis7181@gmail.com>
2024-10-06 05:40:49 +0700

    kode tanpa for in

--- bin/latihan026.dart
+++ bin/latihan026.dart
@@ -1,3 +1,12 @@
 void main() {
   // for in
+  // Kadang kita biasa mengakses data List menggunakann perulangan
+  // Mengakses data List menggunakan perulangan sangat bertele-tele, kita harus membuat counter, lalu mengakses List menggunakan counter yang kita buat
+  // Namun untungnya, terdapat perulangan for in, yang bisa digunakan untuk mengakses seluruh data di List secara otomatis
+
+  // kode tanpa for in
+  var array = <String>['kharis', 'Raihan', '22031013'];
+  for (var i = 0; i <= array.length; i++) {
+    print(array[i]);
+  }
 }
