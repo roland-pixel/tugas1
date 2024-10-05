fcab7087e62593131d4872edb02b6c095bb59aab
roland-pixel <kharis7181@gmail.com>
2024-10-06 05:47:09 +0700

    contoh function

--- bin/latihan027.dart
+++ bin/latihan027.dart
@@ -1,3 +1,16 @@
+void katakan_peta() {
+  print('katakan peta katakan peta');
+}
+
 void main() {
   // function
+  // Function adalah blok kode program yang akan berjalan saat kita panggil
+  // Sebelumnya kita sudah menggunakan method print() untuk menampilkan tulisan di console
+  // Untuk membuat function, kita bisa menggunakan kata kunci void, lalu diikuti dengan nama function, kurung () dan diakhiri dengan block
+  // Kita bisa memanggil function dengan menggunakan nama function lalu diikuti dengan kurung ()
+  // Di bahasa pemrograman lain, Function juga disebut dengan Method
+  katakan_peta();
+  katakan_peta();
+  katakan_peta();
+  katakan_peta();
 }
