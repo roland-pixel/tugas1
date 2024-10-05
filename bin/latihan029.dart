0336c4b332c72815d2d08646ee41761455b38e29
roland-pixel <kharis7181@gmail.com>
2024-10-06 05:58:10 +0700

    kode default value

--- bin/latihan029.dart
+++ bin/latihan029.dart
@@ -2,6 +2,10 @@ void say_hello(String namadepan, [String? namabelakang]) {
   print('hello $namadepan $namabelakang');
 }
 
+void sayhello(String namadepan, [String? namabelakang = '']) {
+  print('hello $namadepan $namabelakang');
+}
+
 void main() {
   // optional parameter
   // Secara default, parameter wajib dikirim ketika kita memanggil function
@@ -9,4 +13,10 @@ void main() {
   // Dan parameter optional haruslah nullable
   say_hello('kharis');
   say_hello('budi', 'setiawan');
+
+  // default value
+  // Jika optional parameter tidak ingin nullable, maka kita wajib menambahkan default value
+  // Caranya, setelah nama parameter, kita tambahkan = default value
+  sayhello('kharis');
+  sayhello('budi', 'setiawan');
 }
