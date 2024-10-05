cf0b54d8e0b7a505ac8e58a811f03bb5874b16cd
roland-pixel <kharis7181@gmail.com>
2024-10-06 05:36:08 +0700

    kode Continue

--- bin/latihan025.dart
+++ bin/latihan025.dart
@@ -14,4 +14,12 @@ void main() {
       break;
     }
   }
+
+  // kode Continue
+  for (var counter = 1; counter <= 100; counter++) {
+    if (counter % 2 == 0) {
+      continue;
+    }
+    print('perulagan ganjil ke $counter');
+  }
 }
