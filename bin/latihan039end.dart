5bfcf9efdc27c3b4f2639ca56a8ac57c1a577b36
roland-pixel <kharis7181@gmail.com>
2024-10-06 14:01:34 +0700

    Kode : Factorial

--- bin/latihan039end.dart
+++ bin/latihan039end.dart
@@ -1,3 +1,15 @@
+// Kode : Factorial
+int factorialloop(int value) {
+  var result = 1;
+  for (var i = 1; i <= value; i++) {
+    result *= i;
+  }
+  return result;
+}
+
 void main() {
   // Recursive Function
+  // Recursive function adalah function yang memanggil function dirinya sendiri
+  // Kadang dalam pekerjaan, kita sering menemui kasus dimana menggunakan recursive function lebih mudah dibandingkan tidak menggunakan recursive function
+  // Contoh kasus yang lebih mudah diselesaikan menggunakan recursive adalah Factorial
 }
