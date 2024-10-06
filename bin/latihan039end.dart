a10dbdb1fc18477a90c4e6083e5ee964f00d99dc
roland-pixel <kharis7181@gmail.com>
2024-10-06 14:06:48 +0700

    Kode : Masalah Dengan Recursive, end.

--- bin/latihan039end.dart
+++ bin/latihan039end.dart
@@ -16,6 +16,20 @@ int factorialRecursive(int value) {
   }
 }
 
+// Walaupun recursive function itu sangat menarik, namun kita perlu berhati-hati
+// Jika recursive terlalu dalam, maka  akan ada kemungkinan  terjadi error StackOverflow, yaitu error dimana stack pemanggilan function terlalu banyak
+// Kenapa problem ini  bisa terjadi? Karena ketika kita memanggil function, Dart akan menyimpannya dalam stack, jika function tersebut memanggil function lain, maka stack akan menumpuk terus, dan jika terlalu dalam, maka stack akan terlalu besar, dan bisa menyebabkan error StackOverflow
+
+// Kode : Masalah Dengan Recursive
+// void loop(int value) {
+//   if (value == 0) {
+//     print('selesai');
+//   } else {
+//     print('loop- $value');
+//     loop(value - 1);
+//   }
+// }
+
 void main() {
   // Recursive Function
   // Recursive function adalah function yang memanggil function dirinya sendiri
