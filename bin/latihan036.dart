afdfb02eac7b2f1ffe93f23784d16ff79bd64509
roland-pixel <kharis7181@gmail.com>
2024-10-06 13:49:55 +0700

    Kode : Anonymous Function as Parameter

--- bin/latihan036.dart
+++ bin/latihan036.dart
@@ -4,6 +4,12 @@ var upperFunction = (String name) {
 };
 var lowerfunction = (String name) => name.toLowerCase();
 
+// Kode : Anonymous Function as Parameter
+void sayhello(String name, String Function(String) filter) {
+  var filteredname = filter(name);
+  print('Hi $filteredname');
+}
+
 void main() {
   // anonymous function
   // Kebanyakan function biasanya memiliki nama, seperti sayHello(), print() dan  lain-lain
@@ -11,6 +17,11 @@ void main() {
   // Pembuatan anonymous function mirip seperti function bisanya, namun yang membedakan adalah tidak ada nama function nya
   // Biasanya anonymous function sering digunakan ketika memanggil function yang membutuhkan parameter berupa function
 
-  print(upperFunction('Kharis'));
-  print(lowerfunction('Raihan'));
+  // print(upperFunction('Kharis'));
+  // print(lowerfunction('Raihan'));
+
+  sayhello('Kharis Raihan 22031013', (name) {
+    return name.toUpperCase();
+  });
+  sayhello('Kharis Raihan 22031013', (String name) => name.toLowerCase());
 }
