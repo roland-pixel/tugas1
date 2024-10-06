4ccaa4bf32090c93960c3164973442d84de9ce3e
roland-pixel <kharis7181@gmail.com>
2024-10-06 13:57:15 +0700

    Kode : Closure

--- bin/latihan038.dart
+++ bin/latihan038.dart
@@ -1,3 +1,14 @@
 void main() {
   // Closure
+  // Closure adalah kemampuan sebuah function atau anonymous function berinteraksi dengan data-data disekitarnya dalam scope yang sama
+  // Harap gunakan fitur closure ini dengan bijak saat kita membuat aplikasi
+  var counter = 0;
+  void increment() {
+    print('Increment');
+    counter++;
+  }
+
+  increment();
+  increment();
+  print(counter);
 }
