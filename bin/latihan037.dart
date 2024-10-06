64d3307f51470028e6ed4c09caebd95b61e3bec6
roland-pixel <kharis7181@gmail.com>
2024-10-06 13:51:17 +0700

    initial commit, scope

---
+++ bin/latihan037.dart
@@ -0,0 +1,6 @@
+void main() {
+  // scope
+  // Variable atau  Function hanya bisa diakses di dalam area dimana mereka dibuat.
+  // Hal ini disebut scope
+  // Contoh, jika sebuah variable dibuat di function, maka hanya bisa diakses di method tersebut, atau jika dibuat didalam block, maka hanya bisa diakses didalam block tersebut
+}
