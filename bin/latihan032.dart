7f145d1be8c6bde24c7d38d6032fdc419845b8ef
roland-pixel <kharis7181@gmail.com>
2024-10-06 11:20:24 +0700

    Kode : Function Short Expression

--- bin/latihan032.dart
+++ bin/latihan032.dart
@@ -1,3 +1,9 @@
+int sum(int first, int second) => first + second;
 void main() {
   // Function Short Expression
+  // Dart mendukung function short expression
+  // Dimana jika terdapat sebuah function yang hanya satu baris, kita bisa menyingkatnya secara sederhana
+  // Untuk membuat  function short expression, kita tidak butuh  kurung  {} dan juga tidak butuh kata kunci return
+  print(sum(12, 5));
+  print(sum(1, 7));
 }
