e2f92be69a5bded35b3339c2704bcdb40f0b6ab2
roland-pixel <kharis7181@gmail.com>
2024-10-06 11:23:06 +0700

    Kode : Inner Function

--- bin/latihan033.dart
+++ bin/latihan033.dart
@@ -1,3 +1,12 @@
 void main() {
   // Inner Function
+  // Di Dart, kita bisa membuat inner  function di dalam outer  function
+  // Namun perlu diperhatikan, inner  function yang dibuat di dalam outer function, hanya bisa  diakses dari outer function saja, tidak bisa diakses dari luar outer function
+  // Untuk lebih detail tentang ini akan  kita bahas di materi tentang Scope
+  void sayhello() {
+    print('hello inner function');
+  }
+
+  sayhello();
+  sayhello();
 }
