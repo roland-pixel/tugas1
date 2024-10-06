a53a5497724c80177b4011d6638a2ec8ccccf300
roland-pixel <kharis7181@gmail.com>
2024-10-06 13:32:34 +0700

    Kode :  Function as Parameter

--- bin/latihan035.dart
+++ bin/latihan035.dart
@@ -1,3 +1,11 @@
+// Kode :  Function as Parameter
+void sayhello(String name, String Function(String) filter) {
+  var filteredname = filter(name);
+  print('Hi $filteredname');
+}
+
 void main() {
   // Higher Order Function
+  // Higher-Order Function adalah function yang menggunakan function sebagai variable, parameter atau return value
+  // Penggunaan Higher-Order Function kadang berguna ketika kita ingin membuat function yang general dan ingin mendapatkan input yang flexible beruba function, yang bisa dideklarasikan oleh pengguna ketika memanggil function tersebut
 }
