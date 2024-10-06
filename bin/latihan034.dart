39e73eb73230dd0dd1e221e2cc5d1f66e1641f2e
roland-pixel <kharis7181@gmail.com>
2024-10-06 11:26:58 +0700

    Kode : Main Function Parameter

--- bin/latihan034.dart
+++ bin/latihan034.dart
@@ -1,3 +1,10 @@
-void main() {
+void main(List<String> args) {
   // main function
+  // Kita sudah tahu dari awal, bahkan main function adalah function yang digunakan sebagai gerbang masuk aplikasi  Dart
+  // Function main adalah function yang akan dijalankan pertama kali oleh Dart
+  // Main function memiliki sebuah parameter optional, yaitu adalah arguments, dimana data parameter tersebut berupa List<String>
+  // Data List<String> tersebut diambil secara otomatis ketika kita menjalankan file dart menggunakan perintah  :
+  // dart namafile.dart arg1 arg2 arg3
+  // dart namafile.dart  “argument 1” “argument  2” “argument 3”
+  print('Arguments : $args');
 }
