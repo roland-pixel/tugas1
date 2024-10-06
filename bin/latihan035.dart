1b8496eaef21781bebeb94a5aca8bd72942a253a
roland-pixel <kharis7181@gmail.com>
2024-10-06 13:40:37 +0700

    Kode  : Higher Order Function

--- bin/latihan035.dart
+++ bin/latihan035.dart
@@ -4,8 +4,19 @@ void sayhello(String name, String Function(String) filter) {
   print('Hi $filteredname');
 }
 
+// Kode  : Higher Order Function
+String filterkatakasar(String name) {
+  if (name == 'gila') {
+    return '****';
+  } else {
+    return name;
+  }
+}
+
 void main() {
   // Higher Order Function
   // Higher-Order Function adalah function yang menggunakan function sebagai variable, parameter atau return value
   // Penggunaan Higher-Order Function kadang berguna ketika kita ingin membuat function yang general dan ingin mendapatkan input yang flexible beruba function, yang bisa dideklarasikan oleh pengguna ketika memanggil function tersebut
+  sayhello('kharis', filterkatakasar);
+  sayhello('gila', filterkatakasar);
 }
