e872d14ec4157101801c35a2fa38e11f279f688e
roland-pixel <kharis7181@gmail.com>
2024-10-06 11:14:18 +0700

    initial commit, function return value

---
+++ bin/latihan031.dart
@@ -0,0 +1,7 @@
+void main() {
+  // function return value
+  // Secara default, function itu menghasilkan value null, namun jika kita ingin, kita bisa membuat sebuah function yang mengembalikan nilai
+  // Agar function bisa menghasilkan value, kita harus mengubah kata kunci void dengan tipe data yang dihasilkan
+  // Dan di dalam block function, untuk menghasilkan nilai tersebut, kita harus menggunakan kata kunci return, lalu diikuti dengan data yang sesuai dengan tipe data yang sudah kita deklarasikan di function
+  // Kita hanya bisa menghasilkan 1 data di sebuah function, tidak bisa lebih dari satu
+}
