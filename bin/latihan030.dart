7cce76fa2ec20fa11ed732fb11ce421a1f268ca3
roland-pixel <kharis7181@gmail.com>
2024-10-06 11:04:37 +0700

    initial commit, Kode Named Parameter

---
+++ bin/latihan030.dart
@@ -0,0 +1,17 @@
+void say_hello({String? namadepan, String? namabelakang}) {
+  print('halo $namadepan $namabelakang');
+}
+
+void main() {
+  // Named Parameter
+  // Secara default, posisi parameter ketika kita memanggil function harus sesuai dengan posisi parameter di function tersebut
+  // Dart memiliki fitur dengan named parameter, dimana saat memanggil parameter kita bisa menyebutkan nama parameter nya, sehingga posisinya tidak perlu harus sesuai dengan posisi parameter nya
+  // Namun ketika membuat function nya, kita perlu melakukan perubahan ketika membuat parameter nya, yaitu dengan menggunakan kurung kurawal {}
+  // Secara default, named parameter adalah nullable, sehingga kita perlu tambahkan karakter ?
+
+  say_hello(namadepan: 'Kharis', namabelakang: 'Raihan');
+  say_hello(namabelakang: 'Nugraha', namadepan: 'Fuji');
+  say_hello();
+  say_hello(namadepan: 'kharis');
+  say_hello(namabelakang: 'Raihan');
+}
