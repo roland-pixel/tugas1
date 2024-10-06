c85547a211125b86eb8b9504038b1c8539eb0979
roland-pixel <kharis7181@gmail.com>
2024-10-06 13:53:36 +0700

    kode : scope

--- bin/latihan037.dart
+++ bin/latihan037.dart
@@ -3,4 +3,14 @@ void main() {
   // Variable atau  Function hanya bisa diakses di dalam area dimana mereka dibuat.
   // Hal ini disebut scope
   // Contoh, jika sebuah variable dibuat di function, maka hanya bisa diakses di method tersebut, atau jika dibuat didalam block, maka hanya bisa diakses didalam block tersebut
+
+  // kode : scope
+  var name = 'Kharis';
+  void sayHello() {
+    var hello = 'hello $name';
+    print(hello);
+  }
+
+  sayHello();
+  // print(hello); // error tidak bisa diakses
 }
